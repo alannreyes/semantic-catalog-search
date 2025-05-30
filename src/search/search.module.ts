2001c4e826277084e4de4d36746ed4292ddcf90a
Alann Reyes <alannreyesj@gmail.com>
2025-05-30 01:57:41 -0500

    fix(logger): corrige clase PostgresTransport para usar winston-transport

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common'; 
 import { SearchService } from './search.service';
+// Si tienes un SearchController, también debería estar importado y listado en 'controllers'
+// import { SearchController } from './search.controller';
 
 @Module({
-  providers: [SearchService],
+  // controllers: [SearchController], // Descomenta si tienes un controlador
+  providers: [
+    SearchService,
+    Logger
+  ],
   exports: [SearchService],
 })
 export class SearchModule {}
